7b940409a1cd760806a0f3640b909bc952330c49
ManikumarJ <manikumarj6@gmail.com>
2026-04-28 13:45:45 +0530

    updated

--- Day-1/task5.js
+++ Day-1/task5.js
@@ -10,13 +10,18 @@
 // c1(); // 2
 // c2(); // 1
 
-function makeCounter() {
- 
+function Counter(start) {
+  let count = start;
+
+  return function () {
+    count++;
+    return count;
+  };
 }
-const c1 = makeCounter();
-const c2 = makeCounter();
 
-c1(); // 1
-c1(); // 2
-c2(); // 1
+const c1 = Counter(0);    
+const c2 = Counter(0);   
 
+console.log(c1());
+console.log(c1()); 
+console.log(c2()); 
